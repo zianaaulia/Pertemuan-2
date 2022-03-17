a23abf3cfbcd43c91851c4e3d4d1ca7d48e1640b
Ziana Aulia <zianaaulia2602@gmail.com>
2022-03-17 11:32:17 +0700

    Kode Program Ketujuh untuk Collection

---
+++ src/main/kotlin/Collections/Ranges.kt
@@ -0,0 +1,16 @@
+package Collections
+/*
+Range di Kotlin adalah tipe unik yang mendefinisikan nilai awal dan nilai akhir.
+Dengan kata lain, ini adalah interval antara awal dan nilai akhir.
+Range di Kotlin ditutup, yang berarti bahwa nilai awal dan nilai akhir termasuk dalam range.
+ */
+fun main(args: Array<String>) {
+//Ranges
+    val i:Int = 2
+    for (j in 1..4)
+        print(j) // prints "1234"
+    println()
+    if (i in 1..10) { // equivalent of 1 <= i && i <= 10
+        println("we found your number --"+i)
+    }
+}
