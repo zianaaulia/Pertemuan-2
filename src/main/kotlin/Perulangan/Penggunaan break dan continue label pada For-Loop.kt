65da1dd830878161bfb1e269de87efe08d4d5f32
Ziana Aulia <zianaaulia2602@gmail.com>
2022-03-17 14:09:14 +0700

    Kode Program Kedelapan untuk Perulangan; Penggunaan Break dan Continue label pada For-Loop

---
+++ src/main/kotlin/Perulangan/Penggunaan break dan continue label pada For-Loop.kt
@@ -0,0 +1,20 @@
+package Perulangan
+/*
+* Pernyataan break digunakan untuk melompat keluar dari loop.
+* Pernyataan continue dapat memecah satu iterasi (dalam loop),
+  jika kondisi tertentu terjadi dan berlanjut dengan iterasi berikutnya dalam loop
+ */
+fun main(args: Array<String>) {
+//Continue & Break Label
+    println("Example of Break and Continue Label")
+    myLabel@ for(x in 1..10) { // appling the custom label
+        if(x == 5) {
+            println("I am inside if block with value"+x+"\n-- hence it will close the operation")
+            break@myLabel //specifing the label
+        } else {
+            println("I am inside else block with value"+x)
+            continue@myLabel
+        }
+        println("Not Print")
+    }
+}
