1989d0cba1a5e82da83fec25e115a1004c6e3983
Ziana Aulia <zianaaulia2602@gmail.com>
2022-03-17 12:08:03 +0700

    Kode Program Pertama untuk Perulangan

---
+++ src/main/kotlin/Perulangan/For Loop.kt
@@ -0,0 +1,24 @@
+package Perulangan
+/*
+Looping adalah sebuah urutan perintah yang secara menerus diulang hingga
+suatu kondisi tercapai.
+ */
+fun main(args: Array<String>) {
+//The for loop
+    for (i in 1..10) {
+        print("$i ")
+    }
+    print('\n')
+    print('\n')
+    val arrInt = arrayOf(1, 2, 3, 4)
+    for (i in arrInt) println("values of the array " +i)
+    print('\n')
+    val listInt = listOf(1, 22, 83, 4)
+    for ((index, value) in listInt.withIndex()) {
+        println("the element at $index is $value")
+    }
+}
+/*
+For loop merupakan statement control flow untuk iterasi khusus, yang memungkinkan kode
+untuk dijalankan berulang.
+ */
