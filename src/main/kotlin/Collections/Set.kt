3526b2454a1eddf4f615ccd55360bb6482c35fe2
Ziana Aulia <zianaaulia2602@gmail.com>
2022-03-17 11:24:22 +0700

    Kode Program Kelima untuk Collection

---
+++ src/main/kotlin/Collections/Set.kt
@@ -0,0 +1,15 @@
+package Collections
+//Set adalah koleksi value yang unik dengan tipe yang sama.
+fun main() {
+//Set<T> menyimpan elemen unik; urutan mereka umumnya tidak terdefinisi.
+    val numbers = setOf(1, 2, 3, 4)
+    println("Number of elements: ${numbers.size}")
+    if (numbers.contains(1)) println("1 is in the set")
+    val numbersBackwards = setOf(4, 3, 2, 1)
+    println("The sets are equal: ${numbers == numbersBackwards}")
+//Implementasi default Set – LinkedHashSet – mempertahankan urutan penyisipan elemen
+    println(numbers.first() == numbersBackwards.first())
+    println(numbers.first() == numbersBackwards.last())
+    val strings = hashSetOf("a", "b", "c", "c") //hash set value unik
+    println("My Set Values are"+strings)
+}
