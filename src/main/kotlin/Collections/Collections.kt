9fdf601c7cc4e521416925b511adda348a8fba10
Ziana Aulia <zianaaulia2602@gmail.com>
2022-03-17 10:37:38 +0700

    Kode Program Pertama untuk Collection

---
+++ src/main/kotlin/Collections/Collections.kt
@@ -0,0 +1,18 @@
+package Collections
+
+fun printAll(strings: Collection<String>) {
+    for(s in strings) print("$s ")
+    println()
+}
+fun main(args: Array<String>) {
+    val stringList = listOf("one", "two", "one")
+    printAll(stringList)
+    val stringSet = setOf("one", "two", "three")
+    printAll(stringSet)
+}
+/*
+Collection adalah konsep umum untuk sebagian besar bahasa pemrograman,
+jadi jika Anda terbiasa dengan, misalnya, koleksi Java atau Python,
+Kita dapa
+t melewati pengantar ini dan melanjutkan ke bagian detail.
+ */
