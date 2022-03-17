bc3f177e947ad66e82e0b74d09c4890a2a0b643f
Ziana Aulia <zianaaulia2602@gmail.com>
2022-03-17 14:33:37 +0700

    Kode Program Pertama untuk Null-Safety

---
+++ src/main/kotlin/Null-Safety/Null-Safety.kt
@@ -0,0 +1,12 @@
+package `Null-Safety`
+
+fun main(args: Array<String>) {
+    //Konsep nilai null
+    //Kode ini tidak bekerja
+    var number =  15
+
+    //number = null //Baris ini akan menyebabkan Erro
+    //Kode diatas error karena null tidak bisa menjadi nilai dari tipe data int
+
+
+}
