4dfae4e831ab42649246be0efafa419b361fff3f
Ziana Aulia <zianaaulia2602@gmail.com>
2022-03-22 23:30:21 +0700

    Kode Program Kelima untuk Null-Safety yaitu Pemeriksaan Kondisi Null

---
+++ src/main/kotlin/Null-Safety/Pemeriksaan Kondisi Null.kt
@@ -0,0 +1,10 @@
+package `Null-Safety`
+
+fun main(args: Array<String>) {
+//Conditions
+    var maybeNumber: Int? = 15
+    if (maybeNumber != null)
+        println(maybeNumber * 2)
+    else
+        println("The entered value isn't a number")
+}
