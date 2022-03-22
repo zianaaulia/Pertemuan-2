974aecd584c0c562a4623475045c301dbf1aca52
Ziana Aulia <zianaaulia2602@gmail.com>
2022-03-22 23:32:25 +0700

    Kode Program Kelima untuk Null-Safety yaitu Pemanggilan yang aman (Safe Calls).

---
+++ src/main/kotlin/Null-Safety/Pemanggilan yang aman (Safe Calls).kt
@@ -0,0 +1,16 @@
+package `Null-Safety`
+
+fun main(args: Array<String>) {
+//Safe calling
+//?.let
+    var maybeNumber: Int? = 15
+    maybeNumber?.let { println(it) }
+//The ?. chaining
+//student?.teacher?.supervisor?.name = "Seymour Skinner"
+}
+/*
+Kotlin memiliki operator panggilan aman (?.) untuk menangani referensi nol.
+Operator ini menjalankan tindakan apa pun hanya ketika referensi memiliki nilai bukan nol.
+Jika tidak, ia mengembalikan nilai nol. Operator panggilan aman
+menggabungkan pemeriksaan nol bersama dengan panggilan metode dalam satu ekspresi.
+ */
